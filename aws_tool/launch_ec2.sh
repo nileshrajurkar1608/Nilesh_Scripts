60550f29440974fef0dc95f35a41e1f23c4fa456
nileshrajurkar1608 <nilesh.rajurkar@gmail.com>
2022-09-16 01:41:58 +0530

    launch_ec2

--- aws_tool/launch_ec2.sh
+++ aws_tool/launch_ec2.sh
@@ -3,3 +3,5 @@
 # AMI_ID="ami-00ff427d936335825"
 
 aws ec2 describe-images  --filters "Name=name,Values=DevOps-LabImage-CentOS7"  | jq
+
+AMI_ID=$(aws ec2 describe-images  --filters "Name=name,Values=DevOps-LabImage-CentOS7"  | jq '.Images[].ImageId' | sed -e 's/"//g')
