8f4bf9df91007bf079e8d86f908221b29925a851
nileshrajurkar1608 <nilesh.rajurkar@gmail.com>
2022-09-16 11:49:42 +0530

    launch

--- aws_tool/launch_ec2.sh
+++ aws_tool/launch_ec2.sh
@@ -10,6 +10,10 @@ AMI_ID=$(aws ec2 describe-images  --filters "Name=name,Values=DevOps-LabImage-Ce
 
 echo "The AMI which we are using is $AMI_ID"
 
-aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro  --security-group-ids ${SGID}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" --instance-market-options "MarketType=spot, SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"| jq
+aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro  --security-group-ids ${SGID}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" --instance-market-options "MarketType=spot, SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"| jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g'
+
+EC2_Public_IP=$(aws ec2 describe-instances --filter "Name=tag-value,Values=$COMPONENT" | jq '.Reservations[].Instances[].NetworkInterfaces[].PrivateIpAddresses[].Association.PublicIp' | sed -e 's/"//g')
+
+echo -e "Public IP address of $COMPONENT is $EC2_Public_IP"
 
 #aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro  --security-group-ids ${SGID}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" --instance-market-options "MarketType=spot, SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"
