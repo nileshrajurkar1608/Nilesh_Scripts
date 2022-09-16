39bae70e331d0deca23fd6b39f6be7b13ff43ad4
nileshrajurkar1608 <nilesh.rajurkar@gmail.com>
2022-09-16 11:06:28 +0530

    launch

--- aws_tool/launch_ec2.sh
+++ aws_tool/launch_ec2.sh
@@ -5,10 +5,11 @@ SGID=sg-08700d26faab1a69d
 
 aws ec2 describe-images  --filters "Name=name,Values=DevOps-LabImage-CentOS7"  | jq
 
+COMPONENT=$1
 AMI_ID=$(aws ec2 describe-images  --filters "Name=name,Values=DevOps-LabImage-CentOS7"  | jq '.Images[].ImageId' | sed -e 's/"//g')
 
 echo "The AMI which we are using is $AMI_ID"
 
-aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro  --security-group-ids ${SGID}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=cart}]" --instance-market-options "MarketType=spot, SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"| jq
+aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro  --security-group-ids ${SGID}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" --instance-market-options "MarketType=spot, SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"| jq
 
 #aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro  --security-group-ids ${SGID}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" --instance-market-options "MarketType=spot, SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"
