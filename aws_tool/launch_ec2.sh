b28ada353f89573ecb19ee0c556f6202fd30ecc4
nileshrajurkar1608 <nilesh.rajurkar@gmail.com>
2022-09-16 12:13:07 +0530

    launch

--- aws_tool/launch_ec2.sh
+++ aws_tool/launch_ec2.sh
@@ -8,7 +8,7 @@ aws ec2 describe-images  --filters "Name=name,Values=DevOps-LabImage-CentOS7"  |
 COMPONENT=$1
 AMI_ID=$(aws ec2 describe-images  --filters "Name=name,Values=DevOps-LabImage-CentOS7"  | jq '.Images[].ImageId' | sed -e 's/"//g')
 
-echo "\e[46;32m The AMI which we are using is $AMI_ID \e[0m"
+echo -e "\e[46;32m The AMI which we are using is $AMI_ID \e[0m"
 
 EC2_Private_IP=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro  --security-group-ids ${SGID}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" --instance-market-options "MarketType=spot, SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"| jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 
