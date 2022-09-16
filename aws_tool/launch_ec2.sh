845d95da9f3775fe25f1bb851531d1e48910b2cc
nileshrajurkar1608 <nilesh.rajurkar@gmail.com>
2022-09-16 12:35:41 +0530

    launch

--- aws_tool/launch_ec2.sh
+++ aws_tool/launch_ec2.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
 # AMI_ID="ami-00ff427d936335825"
+
+#if $1 is empty or not supplied then I want to mark it as failure.
+
+if [-z "$1"] ; then
+    echo -e "\e[41;31m Machine name is missing.Please enter Machine name while running script! \e[0m"
+    exit 1
+fi
+
 SGID=sg-08700d26faab1a69d
-
-aws ec2 describe-images  --filters "Name=name,Values=DevOps-LabImage-CentOS7"  | jq
-
 COMPONENT=$1
+
 AMI_ID=$(aws ec2 describe-images  --filters "Name=name,Values=DevOps-LabImage-CentOS7"  | jq '.Images[].ImageId' | sed -e 's/"//g')
 
 echo -e "\e[46;32m The AMI which we are using is $AMI_ID \e[0m"
@@ -19,6 +25,15 @@ create_server() {
     echo -e "\e[41;31m Public IP address of $COMPONENT is $EC2_Public_IP \e[0m"
 }
 
-create_server
+
+if ["$1" == "all"]; then
+    for component in catalogue cart shipping mongodb payment rabbitmq redis mysql user frontend; do
+    #calling function
+    create_server
+    done
+else
+    create_server
+fi
+
 
 #aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro  --security-group-ids ${SGID}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" --instance-market-options "MarketType=spot, SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"
