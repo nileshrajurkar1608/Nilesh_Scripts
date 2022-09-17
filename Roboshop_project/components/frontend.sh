cba7be0551329e35ac21abd60e9a2ccbee2ad902
nileshrajurkar1608 <nilesh.rajurkar@gmail.com>
2022-09-18 03:41:20 +0530

    roboshop_project

--- Roboshop_project/components/frontend.sh
+++ Roboshop_project/components/frontend.sh
@@ -2,18 +2,25 @@
 
 set -e      # To make sure script stops when any of instruction/command fails.
 
-echo -e "\e[44;32mI am installing frontend:\e[0m"
+echo -e "\e[44;32m I am installing frontend:\e[0m"
 
 source components/common.sh
 
 echo -e "\e[44;32m\n Installing Nginx:\e[0m"
 yum install nginx -y  &>>/tmp/frontend.log
+
+if [ $? -eq 0 ]; then 
+    echo -n -e "Success!"
+else
+    echo -n -e "Failure!"
+fi
+
 systemctl enable nginx
 
 echo -e "\e[44;32m\n Starting Nginx:\e[0m"
 systemctl start nginx 
 
-echo -e "\e[44;32m\nLet's download the HTDOCS content and deploy it under the Nginx path:\e[0m"
+echo -e "\e[44;32m\n Let's download the HTDOCS content and deploy it under the Nginx path:\e[0m"
 
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
 
