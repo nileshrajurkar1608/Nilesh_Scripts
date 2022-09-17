af9fdf0febf3eaad91efcad3a717add735af11e1
nileshrajurkar1608 <nilesh.rajurkar@gmail.com>
2022-09-18 00:41:45 +0530

    bash learning

--- aws_tool/if-else.sh
+++ aws_tool/if-else.sh
@@ -16,21 +16,24 @@
 #     echo -e "You are not a registered User!"
 # fi
 
-# if [ "$NAME" = "Nilesh" ]; then
-#     echo -e "Nilesh, you are a student!"
-# elif [ "$NAME" = "Manu" ]; then
-#     echo -e "Manu, You are a Teacher!"
-# else 
-#     echo -e "\e[46;32mYou are not a registered user!\e[0m"
-# fi
+if [ "$NAME" = "Nilesh" ]; then
+    echo -e "Nilesh, you are a student!"
+    elif [ "$NAME" = "Manu" ]; then
+    echo -e "Manu, You are a Teacher!"
+    else 
+    echo -e "\e[46;32mYou are not a registered user!\e[0m"
+fi
 
-ACTION=$1
+# ACTION=$1
 
+# expect input $1 while running script to make condition true  
 # if [ -z "$ACTION" ]; then
 #     echo -e "Only valid options are start or stop or restart"
 # fi
 
-if [ -n "$ACTION" ]; then
-    echo -e "Does Not expect any input while running script"
-fi
+
+# # doesn't expect input $1 while running script to make condition true 
+# if [ -n "$ACTION" ]; then
+#     echo -e "Does Not expect any input while running script"
+# fi
 
