bd727ea045b62e8e2009c60a3f596c15a4687cc1
nileshrajurkar1608 <nilesh.rajurkar@gmail.com>
2022-09-18 04:10:14 +0530

    roboshop_project

--- Roboshop_project/components/common.sh
+++ Roboshop_project/components/common.sh
@@ -7,10 +7,10 @@ if [ "$ID" -ne 0 ]; then
 fi
 
 status(){
-if [ $1 -eq 0 ]; then 
-    echo -e "\e[32mSuccess!\e[0m"
-else
-    echo -e "\e[31mFailure! Look for logs.\e[0m"
-fi
+    if [ $1 -eq 0 ]; then 
+        echo -e "\e[32mSuccess!\e[0m"
+    else
+        echo -e "\e[31mFailure! Look for logs.\e[0m"
+    fi
 }
 
