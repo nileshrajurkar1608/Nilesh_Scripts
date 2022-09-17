93e9d2a731b1c2f427339ec07e05ccb88599b66a
nileshrajurkar1608 <nilesh.rajurkar@gmail.com>
2022-09-17 21:09:08 +0530

    bash learning

--- aws_tool/bash_basics.sh
+++ aws_tool/bash_basics.sh
@@ -28,4 +28,8 @@ echo -e "Good Morning! Today's date is $DATE_COMMAND"
 
 echo -e "\e[46;31mGood Morning! Today's date is $DATE_COMMAND\e[0m"
 
+echo -e "Name of script you are running is $0"
+
+echo -e "value of c is '$1'"
+
 
