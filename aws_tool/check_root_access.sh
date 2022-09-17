72152308d71bcecd5dd2648cacedc7587ba2d2f2
nileshrajurkar1608 <nilesh.rajurkar@gmail.com>
2022-09-18 01:17:23 +0530

    bash learning

--- aws_tool/check_root_access.sh
+++ aws_tool/check_root_access.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
+# ID=$(id -u)
+# if [ "$ID" != "0" ]; then
+#     echo -e "\e[41;32mPlease run command as a root user!\e[0m"
+#     exit 1
+# fi
+
+# echo -e "\e[42;32mInstalling httpd\e[0m"
+# yum install httpd -y
+
 ID=$(id -u)
-if [ "$ID" != "0" ]; then
-    echo -e "\e[41;32mPlease run command as a root user!\e[0m"
-    exit 1
+
+if [ $ID -eq 0 ]; then
+    echo -e "\e[42;32mInstalling httpd\e[0m"
+    yum install httpd -y
+else
+    echo -e "try running as sudo or root user"
 fi
 
-echo -e "\e[42;32mInstalling httpd\e[0m"
-yum install httpd -y
+
+
 
