785cbccdf7eace76ab02feed330fa97f9a05ed2d
nileshrajurkar1608 <nilesh.rajurkar@gmail.com>
2022-09-18 03:46:26 +0530

    roboshop_project

--- Roboshop_project/components/frontend.sh
+++ Roboshop_project/components/frontend.sh
@@ -2,30 +2,35 @@
 
 set -e      # To make sure script stops when any of instruction/command fails.
 
-echo -e "\e[44;32m I am installing frontend:\e[0m"
+echo -e "\e[32m I am installing frontend:\e[0m"
 
 source components/common.sh
 
 echo -n -e "\e[44;32m\n Installing Nginx:\e[0m"
 yum install nginx -y  &>>/tmp/frontend.log
 
-if [ $? -eq 0 ]; then 
+status(){
+    if [ $? -eq 0 ]; then 
     echo -e "Success!"
 else
     echo -e "Failure!"
 fi
+}
+
+status
 
 systemctl enable nginx
 
-echo -e "\e[44;32m\n Starting Nginx:\e[0m"
+echo -n -e "\e[32m\n Starting Nginx:\e[0m"
 systemctl start nginx 
+status
 
-echo -e "\e[44;32m\n Let's download the HTDOCS content and deploy it under the Nginx path:\e[0m"
+echo -n -e "\e[32m\n Let's download the HTDOCS content and deploy it under the Nginx path:\e[0m"
 
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+status
 
-
-echo -e "\e[44;32m\n Deploy in Nginx Default Location:\e[0m"
+echo -n -e "\e[32m\n Deploy in Nginx Default Location:\e[0m"
 cd /usr/share/nginx/html
 rm -rf *
 unzip -o /tmp/frontend.zip  &>>/tmp/frontend.log
@@ -35,8 +40,12 @@ mv static/* .
 rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 
-echo -e "\e[44;32m\n Restart nginx services:\e[0m"
+status
+
+echo -n -e "\e[32m\n Restart nginx services:\e[0m"
 systemctl restart nginx
+
+status
 
 
 
