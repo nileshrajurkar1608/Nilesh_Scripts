173a612e7eabc53338675abd77613ad3b8e95bb0
nileshrajurkar1608 <nilesh.rajurkar@gmail.com>
2022-09-18 00:19:52 +0530

    bash learning

--- aws_tool/if-else.sh
+++ aws_tool/if-else.sh
@@ -8,3 +8,10 @@ if [ "$NAME" = "Nilesh" ]; then
     echo -e "Welcome Nilesh!"
     exit 1
 fi
+
+if [ "$NAME" = "Manu" ]; then 
+    echo -e "Welcome Manu!"
+    exit 1
+else 
+    echo -e "You are not registered User!"
+fi
