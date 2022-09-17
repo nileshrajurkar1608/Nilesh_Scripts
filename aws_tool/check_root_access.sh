403766c8e5fe103f975f65f3806c32d2d2a28da1
nileshrajurkar1608 <nilesh.rajurkar@gmail.com>
2022-09-18 01:10:24 +0530

    bash learning

--- aws_tool/check_root_access.sh
+++ aws_tool/check_root_access.sh
@@ -2,7 +2,7 @@
 
 ID=$(id -u)
 if [ "$ID" != "0" ]; then
-    echo -e "\e[46;31mPlease login as a root user to run commands!\e[0m"
+    echo -e "\e[41;32mPlease run command as a root user!\e[0m"
     exit 1
 fi
 
