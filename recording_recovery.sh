a6f4ab99f8295bbf3c0ccbf743ae8ee5d4adaab9
nileshrajurkar1608 <nilesh.rajurkar@gmail.com>
2022-09-19 21:46:39 +0530

    roboshop_project

--- recording_recovery.sh
+++ recording_recovery.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
-echo -e "Enter recording ID:"
+echo -n -e "Enter recording ID:"
 read ID
 
 REC_ID=$ID
 
-echo -e "Enter TSM IP:"
+echo -n -e "Enter TSM IP:"
 read TSM
 
 TSM_IP=$TSM
 
-echo -e "Enter MCR URL:"
+echo -n -e "Enter MCR URL:"
 read MCR
 
 MCR_URL=$MCR
 
-/opt/ringcentral/rmp/tsm_get.js '$REC_ID' -d $REC_ID_media --tsm-host $TSM_IP --tsm-port 8080 --mcr-pool $MCR_URL
-
 DOWNLOAD_URL=$(/opt/ringcentral/rmp/tsm_get.js '$REC_ID' -d $REC_ID_media --tsm-host $TSM_IP --tsm-port 8080 --mcr-pool $MCR_URL)
 
 echo -e "$DOWNLOAD_URL"
+
+/opt/ringcentral/rmp/tsm_get.js '$REC_ID' -d $REC_ID_media --tsm-host $TSM_IP --tsm-port 8080 --mcr-pool $MCR_URL
+
+
 
