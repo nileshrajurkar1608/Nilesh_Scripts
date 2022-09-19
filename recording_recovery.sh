4ba2ed3a087707ac777d016601276d3ee4c1be1a
nileshrajurkar1608 <nilesh.rajurkar@gmail.com>
2022-09-20 00:39:12 +0530

    recovery_project

--- recording_recovery.sh
+++ recording_recovery.sh
@@ -3,7 +3,7 @@
 # validating whether the executing user is root or not
 ID=$(id -u)
 if [ $ID -ne 0 ]; then 
-    echo -e "\e[31m Try executing the script with sudo user by entering 'sudo -i' and a password.\n \e[0m"
+    echo -e "\e[41;32m Try executing the script with sudo permissions by entering 'sudo -i' and a password.\e[0m"
     exit 1
 fi 
 
@@ -17,7 +17,7 @@ read TSM
 
 TSM_IP=$TSM
 
-echo -n -e "\e[32mEnter MCR URL:\e[0m "
+echo -n -e "\e[32mEnter MCR URL or IP Address:\e[0m "
 read MCR
 
 MCR_URL=$MCR
@@ -25,6 +25,12 @@ MCR_URL=$MCR
 echo -e "\e[46;32m\nRaw data download URL:\e[0m/opt/ringcentral/rmp/tsm_get.js '${REC_ID}' -d ${REC_ID}_media --tsm-host $TSM_IP --tsm-port 8080 --mcr-pool ${MCR_URL}\n"
 
 /opt/ringcentral/rmp/tsm_get.js ${REC_ID} -d ${REC_ID}_media --tsm-host $TSM_IP --tsm-port 8080 --mcr-pool ${MCR_URL}
+
+echo -n -e "\n\e[32mNumber of downloaded files are: \e[0m"; ls -lrth -R ${REC_ID}_media | wc -l
+
+echo -e "Run below command to 'Transcode raw data' if download of file succeeds.\n"
+
+echo -e "\e[46;32m\nUse below command to 'Transcose' Raw data:\e[0m\nnohup /opt/ringcentral/rmp/bin/tcr -o /mnt/data/${REC_ID}_media.mp4 \"/mnt/data/${REC_ID}_media/${REC_ID}/\" &"
 
 
 
