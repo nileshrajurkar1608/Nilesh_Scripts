d443a9f00c598aebeb38d964ce12ffc30077e27a
nileshrajurkar1608 <nilesh.rajurkar@gmail.com>
2022-09-20 00:06:26 +0530

    recovery_project

--- recording_recovery.sh
+++ recording_recovery.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo -n -e "\e[32mEnter recording ID:\e[0m "
-read ID
+read R_ID
 
-REC_ID=$ID
+REC_ID=$R_ID
 
 echo -n -e "\e[32mEnter TSM IP:\e[0m "
 read TSM
@@ -17,7 +17,7 @@ MCR_URL=$MCR
 
 echo -e "\e[46;32m\nRaw data download URL:\e[0m/opt/ringcentral/rmp/tsm_get.js '${REC_ID}' -d ${REC_ID}_media --tsm-host $TSM_IP --tsm-port 8080 --mcr-pool ${MCR_URL}\n"
 
-/opt/ringcentral/rmp/tsm_get.js \'${REC_ID}\' -d ${REC_ID}_media --tsm-host $TSM_IP --tsm-port 8080 --mcr-pool ${MCR_URL}
+/opt/ringcentral/rmp/tsm_get.js ${REC_ID} -d ${REC_ID}_media --tsm-host $TSM_IP --tsm-port 8080 --mcr-pool ${MCR_URL}
 
 
 
